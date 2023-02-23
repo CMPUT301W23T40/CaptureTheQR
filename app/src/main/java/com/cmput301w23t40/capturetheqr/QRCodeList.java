a4ea87d2dd3e2c06f8b39d15452ae12c03a3a4e8
rachel-ellis <rjellis@ualberta.ca>
2023-02-23 13:16:21 -0700

    fix: add java docs

--- app/src/main/java/com/cmput301w23t40/capturetheqr/QRCodeList.java
+++ app/src/main/java/com/cmput301w23t40/capturetheqr/QRCodeList.java
@@ -12,6 +12,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+/**
+ * This class keeps track of the list of QR Code objects
+ */
+
 public class QRCodeList extends RecyclerView.Adapter<QRCodeList.RecyclerViewHolder>{
 
     private ArrayList<QRCode> qrCodes;
@@ -28,6 +32,9 @@ public class QRCodeList extends RecyclerView.Adapter<QRCodeList.RecyclerViewHold
             url: https://www.geeksforgeeks.org/swipe-to-delete-and-undo-in-android-recyclerview/
             last updated: Oct 5, 2021
     */
+    /**
+     * This class keeps track of and deletes the UI element for displaying QR Codes
+     */
     public class RecyclerViewHolder extends RecyclerView.ViewHolder {
         private TextView qrCodeName;
 
@@ -37,6 +44,12 @@ public class QRCodeList extends RecyclerView.Adapter<QRCodeList.RecyclerViewHold
         }
     }
 
+    /**
+     * This inflates card layout items for the View
+     * @param parent
+     * @param viewType
+     * @return RecylerViewHolder
+     */
     @NonNull
     @Override
     public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -45,12 +58,21 @@ public class QRCodeList extends RecyclerView.Adapter<QRCodeList.RecyclerViewHold
         return new RecyclerViewHolder(v);
     }
 
+    /**
+     * This sets up the item's content in given position used by the View
+     * @param holder
+     * @param position
+     */
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
         QRCode code = qrCodes.get(position);
         holder.qrCodeName.setText(code.getCodeName());
     }
 
+    /**
+     * This returns the length of the RecyclerView
+     * @return size of qr code list
+     */
     @Override
     public int getItemCount() {
         return qrCodes.size();
