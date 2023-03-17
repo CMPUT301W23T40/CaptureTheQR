601930a26336648795ba20a8e457e29db4db34a1
rachel-ellis <rjellis@ualberta.ca>
2023-03-17 14:12:07 -0600

    feat: add scoreboard activity + nav item

---
+++ app/src/main/java/com/cmput301w23t40/capturetheqr/ScoreboardActivity.java
@@ -0,0 +1,48 @@
+package com.cmput301w23t40.capturetheqr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+/**
+ * This class defines the UI page for the QR Code Library
+ */
+public class ScoreboardActivity extends AppCompatActivity {
+
+    /**
+     * override Activity onCreate method
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * override Activity onOptionsItemSelection method for our actionBar back button
+     * @param item
+     * @return boolean
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
